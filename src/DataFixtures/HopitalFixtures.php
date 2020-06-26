5bb3034e26a037fab32add4e79f892631d783f87
sarah <sarah-lambert@live.fr>
2020-06-26 11:02:05 +0200

    ajout fixtures + corrections

--- src/DataFixtures/HopitalFixtures.php
+++ src/DataFixtures/HopitalFixtures.php
@@ -22,15 +22,44 @@ class HopitalFixtures extends Fixture implements DependentFixtureInterface
         $this->addReference('Boobzpital', $hopital);
 
         $hopital = new hopital();
-        $hopital->setNom('Hopital');
+        $hopital->setNom('Hospital');
         $hopital->setAdresse('2 rue du scalpel');
         $hopital->setGps('-69.6959022,-13.6589198');
         $hopital->setTarif(50);
 
         $manager->persist($hopital);
         $hopital->setPay($this->getReference('Thailande'));
-        $this->addReference('Hopital', $hopital);
+        $this->addReference('Hospital', $hopital);
 
+        $hopital = new hopital();
+        $hopital->setNom('Clinique');
+        $hopital->setAdresse('2 rue du bistouri');
+        $hopital->setGps('12.931941,100.9009534');
+        $hopital->setTarif(50);
+
+        $manager->persist($hopital);
+        $hopital->setPay($this->getReference('Thailande'));
+        $this->addReference('Clinique', $hopital);
+
+        $hopital = new hopital();
+        $hopital->setNom('Beauty Hospital');
+        $hopital->setAdresse('2 rue du prank');
+        $hopital->setGps('-3.1190275,-60.0217314');
+        $hopital->setTarif(50);
+
+        $manager->persist($hopital);
+        $hopital->setPay($this->getReference('Bresil'));
+        $this->addReference('Beauty Hospital', $hopital);
+
+        $hopital = new hopital();
+        $hopital->setNom('BoobzBuz');
+        $hopital->setAdresse('2 rue du joke');
+        $hopital->setGps('9.731875299999999,100.0135929');
+        $hopital->setTarif(50);
+
+        $manager->persist($hopital);
+        $hopital->setPay($this->getReference('Thailande'));
+        $this->addReference('BoobzBuz', $hopital);
         $manager->flush();
     }
 
