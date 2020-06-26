1f1ab2517ba472c508146d64b42172e0f6d5283d
chalumeau <chalumeauk@gmail.com>
2020-06-26 09:34:49 +0200

    modif lien accueil navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,11 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/")
+ */
 class HomeController extends AbstractController
 {
     /**
      * Home page display
-     * @Route("index",name="home_index", methods={"GET"})
+     * @Route("/index",name="home_index", methods={"GET"})
      * @return Response A response instance
      */
     public function index() :Response
